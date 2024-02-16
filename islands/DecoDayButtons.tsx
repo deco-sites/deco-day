f74a45630fd5d37ea62d271620fdbffccefa80da
Martin Adam <martin.viana99@gmail.com>
2024-02-15 21:48:24 -0300

    update v1

--- islands/DecoDayButtons.tsx
+++ islands/DecoDayButtons.tsx
@@ -26,7 +26,7 @@ export default function DecoDayButtons({
   return (
     <>
         <div class="flex flex-row items-center justify-center w-full gap-1.5 lg:gap-4 leading-[150%]">
-                {infoPanel.topButtons.map(({ icon, label, url }) => (
+                {infoPanel.topButtons.map(({ icon, label }) => (
                   <button
                     class={`open-button w-1/2 flex items-center justify-center gap-[11px] lg:gap-4 text-[12px] lg:text-[20px] py-4 border rounded-[100px]
                           border-[#FFFFFF26] text-white dark:border-black dark:text-black bg-white bg-opacity-5 hover:opacity-50
